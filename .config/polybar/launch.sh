cb7cc9c9a670c7377c1a6113a664537802115598
adamstuck <adam.moloney.stuck@gmail.com>
2021-08-04 22:20:45 +0200

    Polybar kinda working

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,9 +5,8 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
+# Launch bar1
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
