59e7ee298127e69c8472bb6a0930c5a105f5d12e
Adam <56338480+adastx@users.noreply.github.com>
2021-10-05 21:12:16 +0200

    Neovim setup ported to lua + new plugins, picom edit so focused terminal is opaque

---
+++ .config/nvim/lua/setup.lua
@@ -0,0 +1,24 @@
+local set = vim.opt
+
+set.termguicolors = true
+set.hidden = true
+set.wildmenu = true
+set.showcmd = true
+set.hlsearch = true
+set.ignorecase = true
+set.smartcase = true
+set.incsearch = true
+set.backspace= 'indent,eol,start'
+set.pastetoggle = '<F11>'
+set.confirm = true
+set.relativenumber = true
+set.shiftwidth = 4
+set.softtabstop = 4
+set.autoindent = true
+set.expandtab = true
+set.showmode = false
+set.ruler = false
+
+vim.cmd('colorscheme gruvbox')
+vim.cmd('highlight CursorLineNr cterm=bold ctermfg=Yellow ctermbg=none')
+vim.cmd('highlight Normal ctermbg=none')
