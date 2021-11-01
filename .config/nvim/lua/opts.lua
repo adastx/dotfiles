8ef4655c724b4bd728628abaa3552346a82ec887
Adam <adam.moloney.stuck@gmail.com>
2021-11-01 20:18:08 +0100

    Nvim + omnisharp working with Unity

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -73,7 +73,7 @@ require 'lspconfig'.omnisharp.setup {
     on_attach = function(_, bufnr)
         vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
     end,
-    cmd = { "/usr/bin/omnisharp", "--languageserver" , "--hostPID", tostring(pid) },
+    cmd = { "/home/adam/.cache/yay/omnisharp-roslyn/omnisharp", "--languageserver" , "--hostPID", tostring(pid) },
 }
 
 -- LUA
