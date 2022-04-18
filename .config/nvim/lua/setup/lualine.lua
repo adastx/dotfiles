e95aa362ede74f07db457437cd63c5f12f6954d0
Adam <56338480+adastx@users.noreply.github.com>
2022-04-18 22:55:49 +0200

    nvim: show lsp diagnostics in status bar

--- .config/nvim/lua/setup/lualine.lua
+++ .config/nvim/lua/setup/lualine.lua
@@ -9,7 +9,7 @@ require'lualine'.setup {
     sections = {
         lualine_a = {'mode'},
         lualine_b = {'branch'},
-        lualine_c = {'filename'},
+        lualine_c = {'filename', 'diagnostics'},
         lualine_x = {'encoding', 'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'},
