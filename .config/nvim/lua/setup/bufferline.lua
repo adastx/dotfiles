a64fb392b3793fafd3589935225e8618c174e84c
Adam <adam.moloney.stuck@gmail.com>
2022-05-03 12:36:39 +0200

    nvim: bufferline, lsp mappings

--- .config/nvim/lua/setup/bufferline.lua
+++ .config/nvim/lua/setup/bufferline.lua
@@ -5,7 +5,10 @@ require("bufferline").setup {
             padding = 1,
         } },
         diagnostics = "nvim_lsp",
+        max_name_length = 14,
+        max_prefix_length = 13,
         tab_size = 20,
+        separator_style = "thin",
         show_close_icon = false,
     },
 }
