5b2f12d1ceca8dde4cbd7eae0601e9fe1fd146ee
Adam <56338480+adastx@users.noreply.github.com>
2022-05-13 22:04:25 +0200

    Revert "nvim: Simpler MarkdownPreview lazy-loading"
    This reverts commit 874b40a7a5e2198bba68193c24df2834a8902e94.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -192,6 +192,9 @@ local plugins = {
     {
         'iamcco/markdown-preview.nvim',
         ft = "markdown",
+        run = function()
+            vim.fn['mkdp#util#install']()
+        end,
     },
 }
 
