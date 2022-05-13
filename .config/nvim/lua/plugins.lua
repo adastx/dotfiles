58a73ab989d7f97b4321e6854d5664a9bf1ff944
adamstuck <adam.moloney.stuck@gmail.com>
2022-05-13 12:21:43 +0200

    nvim: Simpler MarkdownPreview lazy-loading

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -192,9 +192,6 @@ local plugins = {
     {
         'iamcco/markdown-preview.nvim',
         ft = "markdown",
-        run = function()
-            vim.fn['mkdp#util#install']()
-        end,
     },
 }
 
