bef40651a883318f28e8121239a3e096a7c3cc95
Adam <adam.moloney.stuck@gmail.com>
2022-05-30 19:36:01 +0200

    nvim: fixed nvim-cmp tab error (unpack)

--- .config/nvim/lua/setup/nvim-cmp.lua
+++ .config/nvim/lua/setup/nvim-cmp.lua
@@ -1,3 +1,7 @@
+if not table.unpack then
+    table.unpack = unpack
+end
+
 local has_words_before = function()
     local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
