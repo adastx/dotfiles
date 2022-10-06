14e9e356c390250349bce9e0d32336126b9e4797
adam <56338480+adastx@users.noreply.github.com>
2022-10-06 20:45:51 +0200

    nvim: simpler lsp indicator

--- .config/nvim/lua/setup/lualine.lua
+++ .config/nvim/lua/setup/lualine.lua
@@ -49,7 +49,8 @@ ins_right {
         for _, client in ipairs(clients) do
             local filetypes = client.config.filetypes
             if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                return '  ' .. client.name
+                -- return '  ' .. client.name
+                return ' '
             end
         end
         return msg
