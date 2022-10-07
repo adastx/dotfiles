ab09544b2bb14fc9555c31b0c603eb726d5a503f
adam <56338480+adastx@users.noreply.github.com>
2022-10-07 15:18:31 +0200

    nvim: changed quickfix mappings, lualine config

--- .config/nvim/lua/setup/lualine.lua
+++ .config/nvim/lua/setup/lualine.lua
@@ -6,7 +6,6 @@ local config = {
         theme = 'onedark',
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
-        disabled_filetypes = { 'startify', 'terminal', 'netrw', 'NvimTree', 'packer' },
         globalstatus = true,
     },
     sections = {
@@ -17,29 +16,14 @@ local config = {
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
     },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
-        lualine_y = {},
-        lualine_z = {},
-    },
-    tabline = {},
-    extensions = {},
 }
 
--- local function ins_left(component)
---     table.insert(config.sections.lualine_c, component)
--- end
-
 local function ins_right(component)
-
     table.insert(config.sections.lualine_x, 1, component)
 end
 
 ins_right {
-    -- Lsp server name .
+    -- LSP Indicator
     function()
         local msg = ''
         local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
@@ -50,13 +34,11 @@ ins_right {
         for _, client in ipairs(clients) do
             local filetypes = client.config.filetypes
             if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                -- return '  ' .. client.name
                 return ' '
             end
         end
         return msg
     end,
-    -- icon = ' ',
 }
 
 lualine.setup(config)
